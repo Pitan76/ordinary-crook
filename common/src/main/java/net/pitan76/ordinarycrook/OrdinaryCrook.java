b32de2a7c65b790a48003c5a4dcd76368992d4cb
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-03-05 16:52:27 +0900

    fix group

--- common/src/main/java/net/pitan76/ordinarycrook/OrdinaryCrook.java
+++ common/src/main/java/net/pitan76/ordinarycrook/OrdinaryCrook.java
@@ -24,7 +24,7 @@ public class OrdinaryCrook {
         WOODEN_CROOK = registry.registerItem(id("wooden_crook"), () -> new BaseCrook(ToolMaterials.WOOD, CompatibleItemSettings.of().addGroup(() -> DefaultItemGroups.TOOLS, id("wooden_crook")), 1, 3.0F));
         BONE_CROOK = registry.registerItem(id("bone_crook"), () -> new BaseCrook(ToolMaterials.STONE, CompatibleItemSettings.of().addGroup(() -> DefaultItemGroups.TOOLS, id("bone_crook")), 2, 3.5F));
         STONE_CROOK = registry.registerItem(id("stone_crook"), () -> new BaseCrook(ToolMaterials.STONE, CompatibleItemSettings.of().addGroup(() -> DefaultItemGroups.TOOLS, id("stone_crook")), 3, 4.0F));
-        BLAZE_ROD_CROOK = registry.registerItem(id("blaze_rod_crook"), () -> new BaseCrook(ToolMaterials.GOLD, CompatibleItemSettings.of().addGroup(DefaultItemGroups.TOOLS, id("blaze_rod_crook")), 5, 4.5F));
+        BLAZE_ROD_CROOK = registry.registerItem(id("blaze_rod_crook"), () -> new BaseCrook(ToolMaterials.GOLD, CompatibleItemSettings.of().addGroup(() -> DefaultItemGroups.TOOLS, id("blaze_rod_crook")), 5, 4.5F));
         WITHERED_BONE_CROOK = registry.registerItem(id("withered_bone_crook"), () -> new BaseCrook(ToolMaterials.IRON, CompatibleItemSettings.of().addGroup(() -> DefaultItemGroups.TOOLS, id("withered_bone_crook")), 7, 5.0F));
 
         registry.allRegister();
